a6995da042044d5fe6bc554c0cd1be809f7a0b12
Jevan006 <jevan.peters@sanlam.co.za>
2025-04-03 07:06:07 +0200

    converted quotes page into a template

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 from sqlalchemy.sql import text
 
 from config import Config
 from extensions import db
+from models.owner import Owner
 from models.pet import Pet
+from routes.auth_bp import auth_bp
 from routes.main_bp import main_bp
 from routes.pet_input_bp import pet_input_bp
 
@@ -14,6 +17,15 @@ def create_app():
 
     # Initialize the DB
     db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = (  # type: ignore
+        "auth_bp.login_page"  # Redirects unauthorized users to the login page
+    )
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Owner.query.get(int(user_id))  # Maintain tokens for specific user
 
     with app.app_context():
         try:
@@ -24,6 +36,7 @@ def create_app():
 
     # Flask - Blueprints
     app.register_blueprint(pet_input_bp)
+    app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
 
     return app
